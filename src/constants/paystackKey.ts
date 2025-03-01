15e30f54fdb3d182317c8ac1a7f0b9532902858a
Teetwothefirst <dexterdavid835@gmail.com>
2025-02-28 16:49:05 -0800

    Paystack Key changed to victor's key

--- src/constants/paystackKey.ts
+++ src/constants/paystackKey.ts
@@ -1,1 +1,2 @@
-export const PaystackKey = "pk_test_86fa1e648f8527d9544ea51fbb2119bc2dd05c17"
+export const PaystackKey = "pk_test_ee85e190e60acfcae735bb1a3806ebc4e614f774"
+// "pk_test_86fa1e648f8527d9544ea51fbb2119bc2dd05c17"
