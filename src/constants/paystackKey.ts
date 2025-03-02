4dec86ed0af973cd9c404b79ed9040f5dd5ab1dd
Teetwothefirst <dexterdavid835@gmail.com>
2025-03-02 10:01:19 -0800

    forgot to change the paystack key to Victor's own

--- src/constants/paystackKey.ts
+++ src/constants/paystackKey.ts
@@ -1,3 +1,3 @@
-export const PaystackKey = "pk_test_86fa1e648f8527d9544ea51fbb2119bc2dd05c17"
+export const PaystackKey = "pk_test_ee85e190e60acfcae735bb1a3806ebc4e614f774"
 // "pk_test_ee85e190e60acfcae735bb1a3806ebc4e614f774" //Victor 
 // "pk_test_86fa1e648f8527d9544ea51fbb2119bc2dd05c17" //Teetwo
