ea9c746b0c75507f8e790bd98e81f5da53cb367b
Krish Acme <acmekrish@gmail.com>
2025-05-05 17:21:18 +0100

    number

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -29,7 +29,7 @@ const Nav = () => {
                     <div className="text">
                       <p>Call anytime</p>
                       <h5>
-                        <a href="tel:123456789">+234 814 780 6378</a>
+                        <a href="tel:+2348078875322">+234 807 887 5322</a>
                       </h5>
                     </div>
                   </li>
