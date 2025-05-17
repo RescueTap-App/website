d7f3307685742e9ff3368c324a87f92e668f5cda
Krish Acme <acmekrish@gmail.com>
2025-05-17 12:21:59 +0100

    window not defined

--- src/app/AppSubscription/page.js
+++ src/app/AppSubscription/page.js
@@ -1,14 +1,15 @@
-import App from "@/components/Subscription";
+import dynamic from 'next/dynamic';
 import Footer from "@/components/Footer";
 import Nav from "@/components/Nav";
 
+const App = dynamic(() => import('@/components/Subscription'), { ssr: false });
+
 export default function Home() {
-  return (
-    <div className="boxed_wrapper ltr">
-    <Nav />
-       <App />
-       <Footer />
-      </div>
-     
-  );
+  return (
+    <div className="boxed_wrapper ltr">
+      <Nav />
+      <App />
+      <Footer />
+    </div>
+  );
 }
